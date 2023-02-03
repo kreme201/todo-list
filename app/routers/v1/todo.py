b3aba01998fd812b8e988ab71d7bd0bb820dcbc9
Kreme201 <kreme201@gmail.com>
2023-02-03 10:24:13 +0900

    Todo API 완성

--- app/routers/v1/todo.py
+++ app/routers/v1/todo.py
@@ -1,31 +1,63 @@
-from typing import List
+from dataclasses import dataclass
+from datetime import datetime
+from typing import Any, List, Union
 
 from fastapi import APIRouter
 
 router = APIRouter(prefix="/todo")
 
-todo_items: List[str] = []
+
+class Todo:
+    id: int
+    status: bool
+    content: str
+    created: datetime
+
+    def __init__(self, content: str):
+        self.id = (
+            max([item.id for item in todo_items]) if len(todo_items) > 0 else 0
+        ) + 1
+        self.status = False
+        self.content = content
+        self.created = datetime.now()
+
+
+todo_items: List[Todo] = []
 
 
 @router.get("/")
-def get_todo_list():
+def get_todo_list() -> List[Todo]:
     return todo_items
 
 
 @router.post("/")
-def create_todo_item(content: str):
-    todo_items.append(content)
+def create_todo_item(content: str) -> Todo:
+    new_item = Todo(content)
+    todo_items.append(new_item)
 
-    return todo_items
+    return new_item
 
 
 @router.put("/{todo_id}")
-def update_todo_item(todo_id: int, content: str):
-    todo_items[todo_id] = content
-    return todo_items
+def update_todo_item(
+    todo_id: int,
+    status: bool,
+) -> Union[Todo, bool]:
+    idx = [item.id for item in todo_items].index(todo_id)
+
+    if idx >= 0:
+        todo_items[idx].status = status
+
+        return todo_items[idx]
+    return False
 
 
 @router.delete("/{todo_id}")
-def delete_todo_item(todo_id: int):
-    del todo_items[todo_id]
-    return todo_items
+def delete_todo_item(todo_id: int) -> bool:
+    idx = [item.id for item in todo_items].index(todo_id)
+    if idx >= 0:
+        del todo_items[idx]
+
+        return True
+
+    return False
